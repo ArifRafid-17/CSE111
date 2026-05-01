329d0b4d12086ebdf864f3d23dac8d5d74869b11
Ariful Islam Rafid <arif.rafid14@gmail.com>
2026-05-02 01:25:42 +0600

    Update BikeDriver.java

--- Lab 7/Assignments/BikeDriver.java
+++ Lab 7/Assignments/BikeDriver.java
@@ -1,5 +1,8 @@
 public class BikeDriver extends GenericDriver {
     String premium;
+    static String [] restrictedAreas = new String [3];
+    static int count=0;
+
 
     public BikeDriver(String a, String b){
         super(a);
@@ -9,6 +12,42 @@ public class BikeDriver extends GenericDriver {
     public String toString(){
         return super.getName()+"'s driver profile is "+premium;
     }
+
+    public static void restrictedAreas(String a){
+        restrictedAreas[count++]=a;
+    }
+
+    public String acceptRide(boolean b){
+        return super.acceptRide(b)+"\n"+toString();
+    }
+
+
+
+    public void fightRestriction(String [] a){
+        boolean flag = false;
+
+        for(int i=0; i<a.length; i++){
+            String temp = a[i];
+
+            for(int j=0; j<count; j++){
+                if(temp.equals(restrictedAreas[j])){
+                    flag= true;
+                    break;
+                }
+            }
+
+            if(flag){
+                System.out.println(getName()+" can not enter "+temp);
+                flag = false;
+            }
+            else{
+                System.out.println(getName()+" can enter "+temp);
+            }
+
+        }
+    }
+
+
 }
 
 
